a3c40b92cdfb6331729fbb026a24b1f6320edb3b
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-06 01:02:01 +0530

    added comments in weather.py and app.py

--- app.py
+++ app.py
@@ -5,21 +5,18 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    data = None 
+    data = None     # Initialize data to None for the initial GET request
 
     if request.method == "POST":
-       
+        # Get data from the form submission
         cityname = request.form["cityname"]
         statename = request.form["statename"]
         countryname = request.form["countryname"]
-
         data = get_weather(cityname, statename, countryname)
-
-  
         print(f"Weather Data Received for {cityname}, {statename}, {countryname}: {data}")
-        
-
+        # Make sure 'temperature' key exists and has a numeric value for display.
     
+    # Render the template, passing the weather data if available
     return render_template("index.html", data=data)
 
 if __name__ == "__main__":
--- weather.py
+++ weather.py
@@ -1,11 +1,13 @@
-import requests
+import requests  #to put requests to API
 from dotenv import load_dotenv
 import os
 from dataclasses import dataclass
 
+#to get API key from .env directly
 load_dotenv()
 apikey = os.getenv("API_KEY")
 
+#to create a structure- like in C
 @dataclass
 class Weatherdata:
     main: str
@@ -13,24 +15,29 @@ class Weatherdata:
     icon: str
     temperature: int
 
+#Function to get Latitude and Longitude to calculate Weather Data
 def getdetails(city_name, state_code, country_code, API_key):
     response = requests.get(
         f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}"
     ).json()
     data = response[0]
-    return data.get("lat"), data.get("lon")
+    latitude=data.get("lat")
+    longitude=data.get("lon")
+    return latitude,longitude
 
+#function to get current weather using latitude and longitude
 def currentweather(latitude, longitude, API_key):
     response = requests.get(
         f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid={API_key}"
     ).json()
     return Weatherdata(
-        main=response.get("weather")[0].get("main"),
+        main=response.get("weather")[0].get("main"),                    #to get exactly the details we need- refered to previous outputs 
         description=response.get("weather")[0].get("description"),
         icon=response.get("weather")[0].get("icon"),
         temperature=int(response.get("main").get("temp"))
     )
 
 def main(cityname, statename, countryname):
-    lat, lon = getdetails(cityname, statename, countryname, apikey)
-    return currentweather(lat, lon, apikey)
+    latitude,longitude = getdetails(cityname,statename,countryname,apikey)
+    currentweatherdata=currentweather(latitude,longitude,apikey)
+    return currentweatherdata
