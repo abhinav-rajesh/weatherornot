502157d7f03ca94d48a39e1a10742b4db288dd2a
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-07 00:01:29 +0530

    change in data source

--- weather_copy.py
+++ weather_copy.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 #to get API key from .env directly
 load_dotenv()
 apikey = os.getenv("API_KEY")
-apikey2 = os.getenv("API_KEY2")
 
 #to create a structure- like in C
 @dataclass
@@ -22,6 +21,7 @@ def getdetails(city_name, state_code, country_code, API_key):
         f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}"
     ).json()
     data = response[0]
+    global latitude,longitude
     latitude=data.get("lat")
     longitude=data.get("lon")
     return latitude,longitude
@@ -39,11 +39,6 @@ def currentweather(latitude, longitude, API_key):
     )
 
 def main(cityname, statename, countryname):
-    #making global copies to pass to forecast
-    global cityname_copy,statename_copy
-    cityname_copy=cityname
-    statename_copy=statename
-
     latitude,longitude = getdetails(cityname,statename,countryname,apikey)
     currentweatherdata=currentweather(latitude,longitude,apikey)
     print(currentweatherdata)
@@ -56,25 +51,37 @@ def main(cityname, statename, countryname):
 #WEATHER FORECASTING!!!!!!!!
 
 from datetime import datetime, timedelta
+import pandas as pd
 
 #fakecall function
 main("Kochi","Kerala","India")
 
 end_date=datetime.today().strftime("%Y-%m-%d")  #fetches todays current date-month-year
 start_date=(datetime.today()-timedelta(days=30)).strftime("%Y-%m-%d") #calculates the date 30 days before today
-location=cityname_copy+","+statename_copy 
-print(location)
 print(start_date)
 print(end_date)
 
-#fetching updated historical data using Visual Crossing
-reqparameters={"unitGroup": "metric","include":"hours","key":apikey2,"contentType":"json"}     #we need the data based on these conditions
-#fetching data from API
-response=requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{start_date}/{end_date}",
-                      params=reqparameters)             
+#fetching updated historical data using Open Meteo
+reqparameters={
+    "latitude":latitude,
+    "longitude":longitude,
+    "start_date":start_date,
+    "end_date":end_date,
+    "hourly": "temperature_2m,precipitation,cloudcover,weathercode",
+    "timezone":"auto"}     #we need the data based on these conditions
+
+#fetching data from SOURCE
+response=requests.get("https://archive-api.open-meteo.com/v1/archive",params=reqparameters)             
 response.raise_for_status()                     #to check if data has been returned, else will raise an exception automatically
 data=response.json()                    #setting up return value as data
 
-print(data)
+
 #Setting up data for each over from data recieved
 datahours=[]
+for day in data["days"]:
+    for hours in data["hours"]:
+        hours["datetime"]=f"{day['datetime']} {hours['datetime']}"
+        datahours.append(hours)
+
+df=pd.DataFrame(datahours)
+print(df)
